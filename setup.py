1c26a6e52c7f4e3f9ff563f8fde67aceb365e441
yarvod <yar.vod.ole@gmail.com>
2022-05-17 15:53:56 +0300

    add module files

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='vna_cals',
+      version='0.1',
+      description='SIS Mixing',
+      packages=['vna_cals'],
+      author_email='yarvodd@gmail.com',
+      zip_safe=False)
