8f065103023b64e9ae37951971832675c1606e2b
yarvod <yar.vod.ole@gmail.com>
2022-08-08 00:55:02 +0300

    rm unused

--- vna_cals/scpi.py
+++ vna_cals/scpi.py
@@ -81,9 +81,6 @@ class Block:
                         logger.warning(f'Exception: {e}; att {at}')
                         continue
 
-                if v == v_from or (2e-3 < v < 3e-3) or (-3e-3 < v < -2e-3):
-                    time.sleep(0.2)
-
                 a_v = 0
                 at=0
                 while at<ats:
@@ -158,39 +155,108 @@ class Block:
     def measure_reflection(self, v_from: float, v_to: float, v_points: int,
                            f_from: float, f_to: float, f_points: int, s_par, exp_path, avg: int):
         refl = defaultdict(list)
+        ats = 10
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((self.HOST, self.PORT))
 
-            s.sendall(bytes(f'BIAS:DEV2:VOLT?', 'utf-8'))    
-            init_v = float(s.recv(1024).decode().rstrip())
+            init_v = 0
+            at=0
+            while at<ats:
+                try:
+                    at += 1
+                    time.sleep(0.3)
+                    s.sendall(b'BIAS:DEV2:VOLT?')
+                    init_v = s.recv(1024)
+                    init_v.decode().rstrip()
+                    init_v = float(init_v)
+                    logger.debug(f'Recieved value: {init_v}; attempt {at}')
+                    break
+                except Exception as e:
+                    logger.warning(f'Exception: {e}; att {at}')
+                    continue
+            init_time = time.time()
             for v in np.linspace(v_from, v_to, v_points):
-                s.sendall(bytes(f'BIAS:DEV2:VOLT {v}', 'utf-8'))
-                if v == v_from or (2.1e-3 < v < 2.5e-3) or (-2.5e-3 < v < -2.1e-3):
-                    time.sleep(0.2)
-                status = s.recv(1024).decode().rstrip()
-                i = 0
-                while 1:
+
+                status = ''
+                at=0
+                while at<ats:
                     try:
-                        time.sleep(0.2)
-                        s.sendall(b'BIAS:DEV2:CURR?')
-                        i = s.recv(1024).decode().rstrip()
-                        i = float(i)
+                        at += 1
+                        s.sendall(bytes(f'BIAS:DEV2:VOLT {v}', 'utf-8'))
+                        status = s.recv(1024)
+                        status.decode().rstrip()
+                        logger.debug(f'Recieved value: {status}; attempt {at}')
                         break
-                    except ValueError:
-                        logger.error(f'Error with v = {v}; i = {i}')
+                    except Exception as e:
+                        logger.warning(f'Exception: {e}; att {at}')
                         continue
 
-                res = get_data(
-                    param=s_par,
-                    plot=False,
-                    plot_phase=False,
-                    freq_start=f_from, freq_stop=f_to,
-                    exp_path=exp_path,
-                    freq_num=f_points or 201,
-                    avg=int(avg)
-                )
-                logger.info(f"volt {v}; curr {float(i)}")
-                refl[f"{v};{i}"] = res['trace']
+                a_v = 0
+                at=0
+                while at<ats:
+                    try:
+                        at += 1
+                        s.sendall(b'BIAS:DEV2:VOLT?')
+                        time.sleep(0.1)
+                        a_v = s.recv(1024)
+                        a_v.decode().rstrip()
+                        a_v = float(a_v)
+                        logger.debug(f'Recieved value: {a_v}; attempt {at}')
+                        break
+                    except Exception as e:
+                        logger.warning(f'Exception: {e}; att {at}')
+                        continue
+
+                i = 0
+                at = 0
+                while at<ats:
+                    try:
+                        at += 1
+                        s.sendall(b'BIAS:DEV2:CURR?')
+                        time.sleep(0.1)
+                        i = s.recv(1024)
+                        i.decode().rstrip()
+                        i = float(i)
+                        logger.debug(f'Recieved value: {i}; attempt {at}')
+                        break
+                    except Exception as e:
+                        logger.warning(f'Exception: {e}; att {at}')
+                        continue
+
+                delta_time = time.time()-init_time
+                res = {}
+                at = 0
+                while at<ats:
+                    try:
+                        at += 1
+                        res = get_data(
+                            param=s_par,
+                            plot=False,
+                            plot_phase=False,
+                            freq_start=f_from, freq_stop=f_to,
+                            exp_path=exp_path,
+                            freq_num=f_points or 201,
+                            avg=int(avg)
+                        )
+                        break
+                    except:
+                        continue
+                refl[f"{a_v};{i}"] = res['trace']
+                logger.info(f"FINISH; time {delta_time}; volt {a_v}; curr {i}")
+
+            status = ''
+            at=0
+            while at<ats:
+                try:
+                    at += 1
+                    s.sendall(bytes(f'BIAS:DEV2:VOLT {init_v}', 'utf-8'))
+                    status = s.recv(1024)
+                    status.decode().rstrip()
+                    logger.debug(f'Recieved value: {status}; attempt {at}')
+                    break
+                except Exception as e:
+                    logger.warning(f'Exception: {e}; att {at}')
+                    continue
 
             s.sendall(bytes(f'BIAS:DEV2:VOLT {init_v}', 'utf-8'))
 
