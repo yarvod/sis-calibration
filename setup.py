e4a544cde123ed759adb097ea98423485ad1178f
yarvod <yar.vod.ole@gmail.com>
2024-02-12 17:13:22 +0300

    master - hjhjh

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(filename):
 
 setup(
     name="sis-calibration",
-    version="0.20",
+    version="0.21",
     description="SIS calibration",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
