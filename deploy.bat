49d053fd155b46e6bcc76ba2d971e45b4dd71294
yarvod <yar.vod.ole@gmail.com>
2024-02-13 14:54:31 +0300

    master - fix auto deploy, add deploy scripts

---
+++ deploy.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+if "%1"=="" (
+    exit /b
+) else (
+    set "Tag=%1"
+)
+
+# Create tags
+git commit --allow-empty -m "Release %Tag%"
+git tag -a %Tag% -m "Version %Tag%"
+
+# Push
+git push upstream --tags
+
+endlocal
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+while getopts "t:" arg; do
+  case $arg in
+    t) Tag=$OPTARG;;
+  esac
+done
+
+# Create tags
+git commit --allow-empty -m "Release $Tag"
+git tag -a $Tag -m "Version $Tag"
+
+# Push
+git push upstream --tags
